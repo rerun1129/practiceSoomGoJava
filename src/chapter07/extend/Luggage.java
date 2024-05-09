a430faa66ada5eb361853d7b16cadba523fb06cb
koyunseok <ysko@ulogitech.com>
2024-05-09 23:17:18 +0900

    7장 1챕터 추가

---
+++ src/chapter07/extend/Luggage.java
@@ -0,0 +1,9 @@
+package chapter07.extend;
+
+public class Luggage {
+    private int size;
+
+    public int getSize ( ) {
+        return size;
+    }
+}
---
+++ src/chapter07/extend/NotEnoughSpaceException.java
@@ -0,0 +1,4 @@
+package chapter07.extend;
+
+public class NotEnoughSpaceException extends Exception {
+}
