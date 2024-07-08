28d2bc7864e37848936de39617a9aac2a2313085
koyunseok <ysko@ulogitech.com>
2024-07-09 00:18:33 +0900

    13장 제네릭 메서드까지 완료

--- src/chapter13/generics_02/box/wildcard/JuicerWild.java
+++ src/chapter13/generics_02/box/wildcard/JuicerWild.java
@@ -18,12 +18,15 @@ public class JuicerWild {
     }
 
     static Juice makeJuiceAll ( FruitBox <? extends Object> fruitBox ) { //모든 타입이 매개변수로 들어올 수 있다.
-        //<?>도 동일한 의미를 가지고 동작한다.
-        //만약 fruitBox 내부의 List 가 Fruit 와 그 자식 타입 변수만 담고 있다면 여기서 문제가 없지만
-        //Fruit 및 자식 타입 이외의 타입 데이터가 들어올 수 있으므로 (여기서 FruitBox는 Object 하위 타입 모두를 담을 수 있음)
-        //캐스팅 예외가 발생할 수 있다. 위와 같은 와일드 카드 선언을 막지 않는 이유는
+        /**
+         * <?>도 동일한 의미를 가지고 동작한다.
+         * 만약 fruitBox 내부의 List 가 Fruit 와 그 자식 타입 변수만 담고 있다면 여기서 문제가 없지만
+         * Fruit 및 자식 타입 이외의 타입 데이터가 들어올 수 있으므로 (여기서 FruitBox는 Object 하위 타입 모두를 담을 수 있음)
+         * 캐스팅 예외가 발생할 수 있다. 위와 같은 와일드 카드 선언을 컴파일 시에 체크하지 않는 이유는 Object 가 Fruit의 공통 조상이기
+         * 때문이며 위의 <? extends Object> 를 <? extends String> 으로 바꾸면 컴파일 에러가 발생한다.
+         * */
         for ( Fruit f : fruitBox.getFruits ()) {
-            System.out.println ( f.toString () );
+            System.out.println ( f.toString () ); //만약 여기서 타입 캐스팅이 실패하면 예외가 발생한다.
         }
         return new Juice ();
     }
